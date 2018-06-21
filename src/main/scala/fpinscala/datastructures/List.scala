bb8e9c383ca0e6592eff820a3c7e257a4c77d492
aneeshd <aneeshd00812@gmail.com>
2018-06-21 23:51:25 +0530

    Solved more exercises from Chapter 3.

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -95,7 +95,7 @@ object List {
   def length[A](as: List[A]): Int = foldRight(as, 0)((_, acc) => acc + 1)
 
   // Exercise 3.10
-  @annotation.tailrec
+  @tailrec
   def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = l match {
     case Nil => z
     case Cons(h, t) => foldLeft(t, f(z, h))(f)
@@ -120,4 +120,109 @@ object List {
   // Exercise 3.14
   def appendUsingFoldRight[A](l1: List[A], l2: List[A]): List[A] =
     foldRight(l1, l2)(Cons(_, _))
+
+  // Exercise 3.15
+  def concat[A](l: List[List[A]]): List[A] =
+    foldRight(l, List[A]())(append)
+
+  // Exercise 3.16
+  def addOne(l: List[Int]): List[Int] = l match {
+    case Nil => Nil
+    case Cons(h, t) => Cons(h + 1, addOne(t))
+  }
+
+  def addOneViaFold(l: List[Int]): List[Int] =
+    foldRight(l, List[Int]())((x, acc) => Cons(x + 1, acc))
+
+  // Exercise 3.17
+  def doubleToString(d: List[Double]): List[String] =
+    foldRight(d, List[String]())((x, acc) => Cons(x.toString, acc))
+
+  // Exercise 3.18
+  def map[A, B](as: List[A])(f: A => B): List[B] = as match {
+    case Nil => Nil
+    case Cons(h, t) => Cons(f(h), map(t)(f))
+  }
+
+  def mapViaFold[A, B](as: List[A])(f: A => B): List[B] =
+    foldRight(as, List[B]())((x, acc) => Cons(f(x), acc))
+
+  def mapUsingBuffer[A, B](as: List[A])(f: A => B): List[B] = {
+    val buf = new collection.mutable.ListBuffer[B]
+
+    @tailrec
+    def loop(l: List[A]): Unit = l match {
+      case Nil => ()
+      case Cons(h, t) => buf += f(h); loop(t)
+    }
+
+    loop(as)
+    List(buf.toList: _*)
+  }
+
+  // Exercise 3.19
+  def filter[A](as: List[A])(f: A => Boolean): List[A] =
+    foldRight(as, List[A]())((x, acc) => if(f(x)) Cons(x, acc) else acc)
+
+  def filterUsingBuffer[A](as: List[A])(f: A => Boolean): List[A] = {
+    val buf = new collection.mutable.ListBuffer[A]
+
+    @tailrec
+    def loop(l: List[A]): Unit = l match {
+      case Nil => ()
+      case Cons(h, t) => if(f(h)) buf += h; loop(t)
+    }
+
+    loop(as)
+    List(buf.toList: _*)
+  }
+
+  // Exercise 3.20
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] =
+    concat(map(as)(f))
+
+  def flatMapUsingFold[A, B](as: List[A])(f: A => List[B]): List[B] =
+    foldRight(as, List[B]())((x, acc) => append(f(x), acc))
+
+  // Exercise 3.21
+  def filterUsingFlatMap[A](as: List[A])(f: A => Boolean): List[A] =
+    flatMap(as)(x => if(f(x)) List(x) else Nil)
+
+  // Exercise 3.22
+  def addRespective(l1: List[Int], l2: List[Int]): List[Int] = (l1, l2) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addRespective(t1, t2))
+  }
+
+  // Exercise 3.23
+  def zipWith[A, B, C](l1: List[A], l2: List[B])(f: (A, B) => C): List[C] = {
+    (l1, l2) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
+    }
+  }
+
+  // Exercise 3.24
+  @tailrec
+  def hasSubSequence[A](sup: List[A], sub: List[A]): Boolean = {
+    @tailrec
+    def beginsWith(s: List[A], t: List[A]): Boolean = (s, t) match {
+      case (_, Nil) => true
+      case (Nil, _) => false
+      case (Cons(h1, t1), Cons(h2, t2)) => if(h1 == h2) beginsWith(t1, t2) else false
+    }
+
+    sup match {
+      case Nil => sub == Nil
+      case l if beginsWith(l, sub) => true
+      case Cons(_, t) => hasSubSequence(t, sub)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val ll = List(List(1, 2, 3), List(4, 5, 6), List(7, 8, 9))
+    println(concat(ll))
+  }
 }
