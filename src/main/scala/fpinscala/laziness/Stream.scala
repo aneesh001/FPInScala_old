2ffc421227c87c34e794b682cdcec0a9107eed86
aneeshd <aneeshd00812@gmail.com>
2018-06-27 00:03:26 +0530

    Finished Chapter 5.

--- src/main/scala/fpinscala/laziness/Stream.scala
+++ src/main/scala/fpinscala/laziness/Stream.scala
@@ -67,7 +67,7 @@ sealed trait Stream[+A] {
 
   // Exercise 5.5
   def takeWhile_1(p: A => Boolean): Stream[A] =
-    foldRight(empty: Stream[A])((x, acc) => if(p(x)) cons(x, acc) else empty)
+    foldRight(empty: Stream[A])((x, acc) => if (p(x)) cons(x, acc) else empty)
 
   // Exercise 5.6
   def headOption_1: Option[A] =
@@ -78,7 +78,7 @@ sealed trait Stream[+A] {
     foldRight(empty[B])((x, acc) => cons(f(x), acc))
 
   def filter(f: A => Boolean): Stream[A] =
-    foldRight(empty[A])((x, acc) => if(f(x)) cons(x, acc) else acc)
+    foldRight(empty[A])((x, acc) => if (f(x)) cons(x, acc) else acc)
 
   def append[B >: A](s: => Stream[B]): Stream[B] =
     foldRight(s)((x, acc) => cons(x, acc))
@@ -88,6 +88,86 @@ sealed trait Stream[+A] {
 
   def find(p: A => Boolean): Option[A] =
     filter(p).headOption
+
+  // Exercise 5.13
+  def map_1[B](f: A => B): Stream[B] = {
+    unfold(this) {
+      case Empty => None
+      case Cons(h, t) => Some((f(h()), t()))
+    }
+  }
+
+  def take_1(n: Int): Stream[A] = {
+    unfold((n, this)) {
+      case (1, Cons(h, t)) => Some((h(), (0, empty)))
+      case (x, Cons(h, t)) => Some((h(), (x - 1, t())))
+      case _ => None
+    }
+  }
+
+  def takeWhile_2(p: A => Boolean): Stream[A] = {
+    unfold(this) {
+      case Cons(h, t) if p(h()) => Some((h(), t()))
+      case _ => None
+    }
+  }
+
+  def zipWith[B, C](s: Stream[B])(f: (A, B) => C): Stream[C] = {
+    unfold((this, s)) {
+      case (Cons(h1, t1), Cons(h2, t2)) => Some((f(h1(), h2()), (t1(), t2())))
+      case _ => None
+    }
+  }
+
+  // Useful to write a general function called zipWithAll.
+  def zipAll[B](s: Stream[B]): Stream[(Option[A], Option[B])] = {
+    unfold((this, s)) {
+      case (Cons(h, t), Empty) => Some(((Some(h()), None), (t(), empty)))
+      case (Empty, Cons(h, t)) => Some(((None, Some(h())), (Empty, t())))
+      case (Cons(h1, t1), Cons(h2, t2)) =>
+        Some(((Some(h1()), Some(h2())), (t1(), t2())))
+      case _ => None
+    }
+  }
+
+  // Exercise 5.14
+  def startsWith[B >: A](s: Stream[B]): Boolean = {
+    zipAll(s).takeWhile {
+      case (_, Some(_)) => true
+      case _ => false
+    }.forAll { case (h1, h2) => h1 == h2 }
+  }
+
+  // Exercise 5.15
+  def tails: Stream[Stream[A]] = {
+    unfold(this) {
+      case Empty => None
+      case s => Some((s, s.drop(1)))
+    } append Stream(empty)
+  }
+
+  def hasSubSequence[B >: A](s: Stream[B]): Boolean =
+    tails exists(_ startsWith s)
+
+  // Exercise 5.16
+
+  // slightly inefficient because if evaluates by name parameter 'acc'
+  // multiple times. Use lazy val to cache the result.
+  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] = {
+    foldRight(empty[B])((x, acc) => acc match {
+      case Empty => cons(f(x, z), Empty)
+      case Cons(h, _) => cons(f(x, h()), acc)
+    })
+  }
+
+  // Implementation taken from textbook site.
+  def scanRight_1[B](z: B)(f: (A, => B) => B): Stream[B] = {
+    foldRight((z, Stream(z)))((x, acc) => {
+      lazy val lacc = acc
+      val res = f(x, lacc._1)
+      (res, cons(res, lacc._2))
+    })._2
+  }
 }
 
 case object Empty extends Stream[Nothing]
